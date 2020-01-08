ad9d991bf99aa643a007ea575382c31feff5e890
danielm2402 <damunoz216@unicauca.edu.co>
2020-01-07 20:36:52 -0500

    componentes basicos traidos de plantilla

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import history from './redux/utils/history'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Route, Switch, BrowserRouter,withRouter, Redirect } from 'react-router-dom';
-import configureStore from './redux/configStore';
-import './App.css';
-//Pages components
-import ComponentPrueba from '../src/components/Component'
+/**
+ * Entry application component used to compose providers and render Routes.
+ *
+ * Note: Because of
+ */
 
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { LastLocationProvider } from "react-router-last-location";
+import { Routes } from "./app/router/Routes";
+import { I18nProvider, LayoutSplashScreen, ThemeProvider } from "./_metronic";
 
-const initialState = {}
-const {persistor, store} = configureStore(initialState, history);
-class App extends Component {
-  render() {
-    return (
-<Provider store={store}>
-<PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter history={ history }>
-        <Route exact path="/" render={()=><Redirect to="/dashboard"/>}/>
-        <Route path="/dashboard" component={ComponentPrueba}/>
-      </BrowserRouter>
-    </PersistGate>
-</Provider>
-    );
-  }
+export default function App({ store, Layout, persistor, basename }) {
+  return (
+    /* Provide Redux store */
+    <Provider store={store} loading={<LayoutSplashScreen />}>
+      {/* Asynchronously persist redux stores and show `SplashScreen` while it's loading. */}
+      <PersistGate persistor={persistor}>
+        {/* Add high level `Suspense` in case if was not handled inside the React tree. */}
+        <React.Suspense fallback={<LayoutSplashScreen />}>
+          {/* Override `basename` (e.g: `homepage` in `package.json`) */}
+          <BrowserRouter basename={basename}>
+            {/*This library only returns the location that has been active before the recent location change in the current window lifetime.*/}
+            <LastLocationProvider>
+              {/* Provide Metronic theme overrides. */}
+              <ThemeProvider>
+                {/* Provide `react-intl` context synchronized with Redux state.  */}
+                <I18nProvider>
+                  {/* Render routes with provided `Layout`. */}
+                  <Routes Layout={Layout} />
+                </I18nProvider>
+              </ThemeProvider>
+            </LastLocationProvider>
+          </BrowserRouter>
+        </React.Suspense>
+      </PersistGate>
+    </Provider>
+  );
 }
-
-export default App;
