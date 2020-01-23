f2928ac3b6cd8dca22b7019ac2422188d277a7d9
danielm2402 <damunoz216@unicauca.edu.co>
2020-01-23 10:43:01 -0500

    true loading components

--- src/app/pages/Revisar/Revisar.js
+++ src/app/pages/Revisar/Revisar.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
-
-export default class Revisar extends Component {
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
+import {getDemandados, getEmbargo} from '../../redux/actions/embargosAction'
+class Revisar extends Component {
     componentDidMount(){
         console.log(this.props)
+        this.props.handleEmbargo(this.props.match.params.id, this.props.token)
+        this.props.handleDemandados(this.props.match.params.id, this.props.token)
     }
     render() {
         return (
             <div>
-                {}
-                Hola mundo
+                {this.props.loadingEmbargo||this.props.loadingDemandados?
+                <div>LOADINGGGG</div>:
+                <div>Hola mundoOOO</div>}
             </div>
         )
     }
 }
+
+
+const mapStateToProps=(state)=>({
+    token: state.auth.authToken,
+    loadingEmbargo: state.EmbargosReducer.embargo.loading,
+    loadingDemandados: state.EmbargosReducer.demandados.loading,
+})
+const mapDispatchToProps=(dispatch)=>({
+    handleEmbargo: bindActionCreators(getEmbargo, dispatch),
+    handleDemandados:bindActionCreators(getDemandados, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Revisar)
