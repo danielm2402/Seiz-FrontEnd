09e46d7f1160b31aa1f87bc28a69b9c7123dfb17
danielm2402 <damunoz216@unicauca.edu.co>
2020-02-18 03:19:38 -0500

    nuevo algoritmo update element demandando

--- src/app/redux/modules/reducers/EmbargosReducer.js
+++ src/app/redux/modules/reducers/EmbargosReducer.js
@@ -1,101 +1,117 @@
 
-import {GET_DEMANDADOS_SUCCESS, GET_EMBARGO_SUCCESS, GET_EMBARGOS_CONFIRMADOS,GET_EMBARGO, GET_DEMANDADOS, GET_EMBARGOS_CONFIRMADOS_SUCCESS, GET_EMBARGOS_POR_CONFIRMAR, GET_EMBARGOS_POR_CONFIRMAR_SUCCESS,
-GET_EMBARGOS_ASIGNADOS, GET_EMBARGOS_ASIGNADOS_SUCCESS, GET_EMBARGOS_ALL, GET_EMBARGOS_ALL_SUCCESS,UPDATE_DEMANDADO, UPDATE_DEMANDANTE
+import {
+    GET_DEMANDADOS_SUCCESS, GET_EMBARGO_SUCCESS, GET_EMBARGOS_CONFIRMADOS, GET_EMBARGO, GET_DEMANDADOS, GET_EMBARGOS_CONFIRMADOS_SUCCESS, GET_EMBARGOS_POR_CONFIRMAR, GET_EMBARGOS_POR_CONFIRMAR_SUCCESS,
+    GET_EMBARGOS_ASIGNADOS, GET_EMBARGOS_ASIGNADOS_SUCCESS, GET_EMBARGOS_ALL, GET_EMBARGOS_ALL_SUCCESS, UPDATE_DEMANDADO, UPDATE_DEMANDANTE
 } from '../../constants/EmbargosConst';
 
 export const Authstate = {
-    loading:true,
-    confirmados:[],
-    porConfirmar:[],
-    asignados:[],
-    all:[],
-    embargo:{loading:true, data:{plaintiffs:[]}, document:null, json:null, json1:null},
-    demandados:{loading:true, data:[]}
-    
+    loading: true,
+    confirmados: [],
+    porConfirmar: [],
+    asignados: [],
+    all: [],
+    embargo: { loading: true, data: { plaintiffs: [] }, document: null, json: null, json1: null },
+    demandados: { loading: true, data: [] }
+
 }
-export default function authReducer(state = Authstate, action={}){
+export default function authReducer(state = Authstate, action = {}) {
     switch (action.type) {
         case GET_EMBARGOS_CONFIRMADOS:
-            return{
+            return {
                 ...state,
-                loading:true,
+                loading: true,
             };
         case GET_EMBARGOS_CONFIRMADOS_SUCCESS:
-            return{
+            return {
                 ...state,
-                loading:false,
-                confirmados:action.data
+                loading: false,
+                confirmados: action.data
             };
         case GET_EMBARGOS_POR_CONFIRMAR:
-            return{
+            return {
                 ...state,
-                loading:true,
-            }  
+                loading: true,
+            }
         case GET_EMBARGOS_POR_CONFIRMAR_SUCCESS:
-            return{
+            return {
                 ...state,
-                loading:false,
+                loading: false,
                 porConfirmar: action.data
             }
         case GET_EMBARGOS_ASIGNADOS:
-            return{
+            return {
                 ...state,
-                loading:true,
-            }  ;
+                loading: true,
+            };
         case GET_EMBARGOS_ASIGNADOS_SUCCESS:
-            return{
+            return {
                 ...state,
-                loading:false,
-                asignados:action.data
+                loading: false,
+                asignados: action.data
             }
         case GET_EMBARGOS_ALL:
-            return{
+            return {
                 ...state,
-                loading:true
-            }   
+                loading: true
+            }
         case GET_EMBARGOS_ALL_SUCCESS:
-            return{
+            return {
                 ...state,
-                all:action.data
+                all: action.data
             }
         case GET_EMBARGO:
-            return{
+            return {
                 ...state,
-                embargo:{loading:true, data:{}}
+                embargo: { loading: true, data: {} }
             }
         case GET_DEMANDADOS:
-            return{
+            return {
                 ...state,
-                demandados:{loading:true,data:[]}
+                demandados: { loading: true, data: [] }
             }
         case GET_DEMANDADOS_SUCCESS:
-            return{
+            return {
                 ...state,
-                demandados:{loading:false, data:action.data}
-            } 
+                demandados: { loading: false, data: action.data }
+            }
         case GET_EMBARGO_SUCCESS:
-            return{
+            return {
                 ...state,
-                embargo:{loading:false, data:action.data, document:action.document, json:action.json, json1:action.json1}
-            }  
+                embargo: { loading: false, data: action.data, document: action.document, json: action.json, json1: action.json1 }
+            }
         case UPDATE_DEMANDADO:
-            return{
+            return {
                 ...state,
-                demandados:{loading:false, data: [...state.demandados.data.filter(item => item.id!==action.id ), {...state.demandados.data.find(element => element.id === action.id),nombres:action.data.nombres,
-                tipoIdentificacion:action.data.tipoIdentificacion,
-                identificacion:action.data.identificacion,
-                montoAEmbargar:action.data.montoAEmbargar
-                }]}
-            } 
-            case UPDATE_DEMANDANTE:
-                return{
-                    ...state,
-                    embargo:{...state.embargo, data:{...state.embargo.data, plaintiffs:[...state.embargo.data.plaintiffs.filter(item => item.id!==action.id),{...state.embargo.data.plaintiffs.find(element => element.id === action.id),fullname:action.data.nombres,
-                        identificacion:action.data.identificacion
-                    
-                        }]}}
+                demandados: {
+                    loading: false, data:state.demandados.data.map(item => {
+                        if (item.id === action.id) {
+                            return {
+                                ...item,
+                                nombres: action.data.nombres,
+                                tipoIdentificacion: action.data.tipoIdentificacion,
+                                identificacion: action.data.identificacion,
+                                montoAEmbargar: action.data.montoAEmbargar
+                            }
+                          
+                        }
+                        return item
+                    })
+                }
+            }
+        case UPDATE_DEMANDANTE:
+            return {
+                ...state,
+                embargo: {
+                    ...state.embargo, data: {
+                        ...state.embargo.data, plaintiffs: [...state.embargo.data.plaintiffs.filter(item => item.id !== action.id), {
+                            ...state.embargo.data.plaintiffs.find(element => element.id === action.id), fullname: action.data.nombres,
+                            identificacion: action.data.identificacion
 
-                }                      
+                        }]
+                    }
+                }
+
+            }
 
         default:
             return state;
